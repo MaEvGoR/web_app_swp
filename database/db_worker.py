2be1b17b899284861a8195a050b214c2a2e48c7a
Maxim Evgrafov <maxim.evgrafov@gmail.com>
2020-03-16 00:57:47 +0300

    log in small
    first log in trial

--- database/db_worker.py
+++ database/db_worker.py
@@ -1,9 +1,27 @@
-from redis import Redis
+# from redis import Redis
 
-rd = Redis(host='redis', db=0)
+# rd = Redis(host='redis', db=0)
 
-def get_last_requests():
-	return rd.get('requests')
+# def get_last_requests():
+# 	return rd.get('requests')
 
-def add_request():
-	rd.set('requests', int(rd.get('requests'))+1)
+# def add_request():
+# 	rd.set('requests', int(rd.get('requests'))+1)
+
+
+
+# test data generated manually
+test_data = [
+{3238:{'login':'m.evgrafov@innopolis.university','password':'0'}},
+{3318:{'login':'r.talalaeva@innopolis.university','password':'1'}},
+{3258:{'login':'r.muraviev@innopolis.university','password':'2'}},
+{3108:{'login':'i.ishbaev@innopolis.university','password':'3'}}
+]
+
+
+
+def get_login_password_info():
+
+	# here some actions to get data from database
+
+	return test_data
--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from database import db_worker
+from backend import be_worker
 
 app = Flask(__name__)
 
@@ -12,16 +12,29 @@ def home():
 
 @app.route('/login', methods=['GET'])
 def login_page():
-	return render_template("login_page.html", login_ins=db_worker.get_last_requests())
+	return render_template("login_page.html")
 
 @app.route('/log_in', methods=['POST'])
 def log_in():
 	login_text = request.form['login']
 	password_text = request.form['password']
 	print("{}:{}".format(login_text, password_text))
-	db_worker.add_request()
 
-	return redirect(url_for('home'))
+	if be_worker.login_password_verification(login_text, password_text):
+		return redirect(url_for('base'))
+	else:
+		return redirect(url_for('error'))
+
+
+@app.route('/base', methods=['GET'])
+def base():
+	return render_template("base.html")
+
+
+@app.route('/error', methods=['GET'])
+def error():
+	return render_template("error.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
